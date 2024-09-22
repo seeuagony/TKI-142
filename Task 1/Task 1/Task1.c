c391823d04f876fd545196f5a8aa9bacb32852aa
seeuagony <163781660+seeuagony@users.noreply.github.com>
2024-09-22 23:30:09 +0300

    Update Task1.c
    исправил ошибки

--- Task 1/Task 1/Task1.c
+++ Task 1/Task 1/Task1.c
@@ -5,27 +5,24 @@
 * @brief рассчитывает выражение A по формуле
 * @param x - значение константы х
 * @param y - значение константы y
-* @param a - значение константы a
+* @param a - значение константы z
 * @return рассчитанное значение
 */
-
 double getA(double const x, double const y, double const z);
 
 /**
 * @brief рассчитывает выражение B по формуле
 * @param x - значение константы х
 * @param y - значение константы y
-* @param a - значение константы a
+* @param a - значение константы z
 * @return рассчитанное значение
 */
-
 double getB(double const x, double const y, double const z);
 
 /**
 * @brief Точка входа в программу.
 * @return 0, в случае успеха.
 */
-
 int main() {
 
 	const double x = 0.3;
@@ -35,13 +32,11 @@ int main() {
 	printf("%f", getB(x, y, z));
 
 	return 0;
-
-
 }
 
-double getA(double const x, double const y, double const z) {
+double getA(const double x, const double y, const double z) {
 	return ((pow(z, 2) * x) + ((pow(exp(1.0), -x)) * (cos(y * x)))) / y*x - pow(exp(1.0), -x)*sin(y*x) + 1;
 }
-double getB(double const x, double const y, double const z) {
+double getB(const double x, const double y, const double z) {
 	return (pow(exp(1.0), 2 * x) * log(z + x)) - (pow(y, 3 * x) * log(y - x));
 }
