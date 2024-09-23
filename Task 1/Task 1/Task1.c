7608ec4a5e21869266e79aaee9da2121534ebab7
seeuagony <desire2desu@yandex.com>
2024-09-23 16:54:27 +0300

    Решение задания 1.2

--- Task 1/Task 1/Task1.c
+++ Task 1/Task 1/Task1.c
@@ -1,32 +1,44 @@
 ﻿#include <stdio.h>
+
 /**
-* @brief рассчитывает выражение getDistance по формуле
+* @brief рассчитывает дистанцию, которую прошла лодка
 * @param boatSpeed - значение константы скорости лодки
 * @param riverSpeed - значение константы скорости реки
 * @param time - значение константы времени
 * @return рассчитанное значение
 */
-double getDistance(const double boatSpeed, const double riverSpeed, const int time);
+double getDistance(const double boatSpeed, const double riverSpeed, const double time);
+
+/**
+* @brief считывает вещественное число
+* @return возвращает вещественное число
+*/
+double Input();
 
 /**
 * @brief Точка входа в программу.
 * @return 0, в случае успеха.
 */
 int main() {
-    double boatSpeed, riverSpeed;
-    int time;
     printf("Enter boat speed (km/h): ");
-    scanf_s("%lf", &boatSpeed);
+    double boatSpeed = Input();
     printf("Enter river speed (km/h): ");
-    scanf_s("%lf", &riverSpeed);
+    double riverSpeed = Input();
     printf("Enter total travel time (hours): ");
-    scanf_s("%d", &time);
-    double distance = getDistance(boatSpeed, riverSpeed, time);
+    double time = Input();
     printf("Total travel: %.2f km\n", getDistance(boatSpeed, riverSpeed, time));
 
     return 0;
 }
 
-double getDistance(double boatSpeed, double riverSpeed, int time) {
+double getDistance(const double boatSpeed, const double riverSpeed, const double time) {
     return ((riverSpeed + boatSpeed) * time);
+}
+double Input() {
+    double value = 0.0;
+    int result = scanf_s("%lf", &value);
+    if (result != 1) {
+        perror("Invalid input!");
+    }
+    return value;
 }
